6260b46c4d079bc9836f6c2e111c1b655f09d46d
Roman Isaev <ir@1313.ru>
2022-01-12 02:31:02 +0300

    Добавлены цыклы

--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,7 +1,27 @@
 public class Cycle{
 	public static void main(String[] args){
-		for(int i=0;i<20;i++){
-			System.out.println(i);
+		System.out.println("for----------------------");
+		for(int i=0;i<=20;i++){
+			System.out.print(i+" ");
 		}
+		System.out.println("");
+		int cnt=6;
+		System.out.println("while----------------------");
+		while(cnt>=-6)
+		{
+        	System.out.print(cnt+" ");
+  			cnt-=2;
+
+		}
+		System.out.println("");
+		System.out.println("do-while----------------------");
+		int result=0;
+		int bufNumb =9;
+		do{
+			bufNumb+=2;
+		    result+=bufNumb;
+		  }	  
+		 while(bufNumb<=20);
+System.out.println(result);
 	}
 }
