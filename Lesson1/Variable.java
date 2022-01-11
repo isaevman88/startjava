9d6bc28dbffc15ca603b6aa9f13f20e3eacdef61
Roman Isaev <ir@1313.ru>
2022-01-12 00:13:10 +0300

    Добавден превый урок (Переменные)

---
+++ Lesson1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable{
+	 public static void main(String[] args) {
+		byte byteVar=1;
+		short s=8;
+		int i=37234;
+		long l=12987789;
+		float f=34.77f;
+		double d=564.28323d;
+		boolean isTrue=false;
+		char c='C';
+		System.out.println(byteVar);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(isTrue);
+		System.out.println(c);
+	}
+}
