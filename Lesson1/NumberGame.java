00004021fddeb5e5ec490bc89cd450bf8a622062
Roman Isaev <ir@1313.ru>
2022-01-19 17:36:20 +0300

    игра Угадай число

---
+++ Lesson1/NumberGame.java
@@ -0,0 +1,29 @@
+public class NumberGame{
+	public static void main(String[] args){
+		System.out.println("Игра угадай число");
+		int Answer=34;
+		int PlayerNumb=47;
+		while(Answer!=PlayerNumb){
+			System.out.println("Число игрока"+PlayerNumb);
+			if(PlayerNumb>Answer)
+			{
+				System.out.println("Данное число больше того, что загадал компьютер");
+				PlayerNumb--;
+			}
+			if(PlayerNumb<Answer)
+			{
+				System.out.println("Данное число меньше того, что загадал компьютер");
+				PlayerNumb++;
+			}
+			
+		}
+		if(PlayerNumb==Answer)
+			{System.out.println("Поздравляю, число угадано! "+Answer);}
+				
+		
+		
+
+			
+		}
+
+	}
